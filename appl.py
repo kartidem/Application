f9f40f17e6cfd91427a51cd2387d3997d060632c
kartidem <karttikeyaidemia@gmail.com>
2024-07-17 12:08:03 +0530

    removed Thrift

--- appl.py
+++ appl.py
@@ -13,6 +13,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from htmlTemplates import css, bot_template, user_template
 
+
 class TextExtractionException(Exception):
     pass
 class FileNotFoundException(Exception):
