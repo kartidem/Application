4256c065e10f2ef615b1dd7dfddbc18d1193a21b
kartidem <karttikeyaidemia@gmail.com>
2024-07-19 11:34:19 +0530

    Update appl.py

--- appl.py
+++ appl.py
@@ -146,6 +146,7 @@ def main():
             }
         </style>
         <img src="https://github.com/kartidem/Application/blob/main/IDEMIAlogoTransparent.png?raw=true" id="idemia-logo">
+        
         """
         st.markdown(logo_html, unsafe_allow_html=True)
 
